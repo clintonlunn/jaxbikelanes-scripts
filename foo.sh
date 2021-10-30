263a54d97b410373c7054ba51e04619686221e43
clintonlunn <cpl09c@gmail.com>
2021-10-29 23:54:57 -0400

    it works with moo calling foo shhhhh

---
+++ foo.sh
@@ -0,0 +1,15 @@
+query-overpass << EOF
+    [out:json] [timeout:25];
+    area(3600119008) -> .area_0;
+    (
+        nwr["highway"="cycleway"](area.area_0);
+        nwr["cycleway"](area.area_0);
+        nwr["cycleway:left"](area.area_0);
+        nwr["cycleway:right"](area.area_0);
+        nwr["bicycle_road"="yes"](area.area_0);
+        nwr["bicycle_parking"](area.area_0);
+        nwr["bicycle"="designated"](area.area_0);
+    );
+    (._;>;);
+out body;
+EOF
